7e5cb16028e985ecae063489461d3b02e406a63e
Ian Maloney <85586358+ianmaloney14@users.noreply.github.com>
2022-11-18 02:42:25 -0800

    arrays

---
+++ arraysToDo/arraysToDo1.js
@@ -0,0 +1,13 @@
+let nums = [1, 2, 3, 4, 5, 6]
+// Push To Front
+nums.unshift(9)
+console.log(nums)
+
+// Pop Front
+nums.shift()
+console.log(nums)
+
+// Insert At
+nums.splice(4, 0, 12)
+console.log(nums)
+
